6c223826452169b943d29d3dad6b0cdcb0c554fd
Alan Reskin <alanreskin@Alans-MacBook-Pro.local>
2022-02-20 14:38:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Property } from "./components/Property";
+import { useFetchProps } from "./hooks/useFetchProps";
+import useSound from "use-sound";
+import beep from "./assets/beep.mp3";
 
-function App() {
+export const App = () => {
+  const [play] = useSound(beep);
+  let [lastPropsId, setLastPropsId] = useState([]);
+
+  const sameLengthDiffProp = (a, b) => {
+    a.sort();
+    b.sort();
+    for (let i = 0; i < a.length; i++) {
+      if (b[i] !== a[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const state = useFetchProps();
+
+  useEffect(() => {
+    setLastPropsId(state.data);
+    console.log("LAST PROPS ID", lastPropsId);
+    console.log("LAST PROPS ID length", lastPropsId.length);
+    console.log("state data length ", state.data.length);
+
+    if (
+      state.data.length !== 0 &&
+      (state.data.length !== lastPropsId.length ||
+        sameLengthDiffProp(state.data, lastPropsId))
+    ) {
+      alert("Se encontraron nuevas propiedades");
+      play();
+      setLastPropsId(state.data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {state.loading ? (
+        <div> CARGANDO </div>
+      ) : (
+        <>
+          <small className="text-muted">
+            Updated: {new Date(Date.now()).toLocaleString().split(",")[1]}
+          </small>
+          <p className="card-text">
+            <small className="text-muted">
+              Resultados obtenidos: {state.data.length}
+            </small>
+          </p>
+          {state.data.length > 0 ? (
+            <>
+              <div className="card-columns">
+                {state.data &&
+                  state.data.map((prop) => {
+                    return <Property {...prop} key={prop.id} />;
+                  })}
+              </div>
+            </>
+          ) : (
+            <div>No se encontraron resultados</div>
+          )}
+        </>
+      )}
+    </>
   );
-}
-
-export default App;
+};
